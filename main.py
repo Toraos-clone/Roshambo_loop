b0d3139384b45a0bb3a7159efd2cf5b08e312130
Will Knight <will.knight.1@gmail.com>
2021-02-26 00:08:11 +0000

    Added a comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 userChoice = ""
 gameOver = False
-
+#setting the loop
 while not gameOver or userChoice != 4:
   userChoice = int(input("Select 1 for Rock, 2 for Paper, 3 for Scissors or 4 to Quit: "))
   if userChoice == 4:
